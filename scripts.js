2862cf1624227e7230ca5f8a60e115a3e69db85b
Abrielle <125398766+abrielleperry@users.noreply.github.com>
2024-07-22 14:22:04 -0500

    popular section

--- scripts.js
+++ scripts.js
@@ -1,42 +1,49 @@
 $(document).ready(function () {
-  $("#loader").show();
+  $(".loader").show();
+  $("#quote-carousel").addClass("d-none");
 
   $.ajax({
     url: "https://smileschool-api.hbtn.info/quotes",
     method: "GET",
-    success: function (data) {
-      $("#loader").hide();
+    success: function (response) {
+      $(".loader").hide();
+      $("#quote-carousel").removeClass("d-none");
 
-      data.forEach(function (quote) {
-        $("#quotes-carousel").append(`
-                        <div class="carousel-item">
-                            <div class="row mx-auto align-items-center">
-                                <div class="col-12 col-sm-2 col-lg-2 offset-lg-1 text-center">
-                                    <img src="${quote.pic_url}" class="d-block align-self-center" alt="Carousel Pic">
-                                </div>
-                                <div class="col-12 col-sm-7 offset-sm-2 col-lg-9 offset-lg-0">
-                                    <div class="quote-text">
-                                        <p class="text-white">"${quote.text}"</p>
-                                        <h4 class="text-white font-weight-bold">${quote.name}</h4>
-                                        <span class="text-white">${quote.title}</span>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    `);
+      response.forEach((quote, index) => {
+        const carouselItem = `
+          <div class="carousel-item ${index === 0 ? "active" : ""}">
+            <div class="row mx-auto align-items-center">
+              <div class="col-12 col-sm-2 col-lg-2 offset-lg-1 text-center">
+                <img src="${
+                  quote.pic_url
+                }" class="d-block align-self-center" alt="Carousel Pic ${
+          index + 1
+        }" />
+              </div>
+              <div class="col-12 col-sm-7 offset-sm-2 col-lg-9 offset-lg-0">
+                <div class="quote-text">
+                  <p class="text-white">« ${quote.text}</p>
+                  <h4 class="text-white font-weight-bold">${quote.name}</h4>
+                  <span class="text-white">${quote.title}</span>
+                </div>
+              </div>
+            </div>
+          </div>
+        `;
+        $("#quote-carousel .carousel-inner").append(carouselItem);
       });
 
-      $("#quotes-carousel").slick({
-        slidesToShow: 1,
-        prevArrow:
-          '<button type="button" class="slick-prev"><img src="images/arrow_white_left.png" alt="Previous quote" aria-hidden="true"/></button>',
-        nextArrow:
-          '<button type="button" class="slick-next"><img src="images/arrow_white_right.png" alt="Next quote" aria-hidden="true"/></button>'
+      $("#quote-carousel").slick({
+        prevArrow: $(".carousel-control-prev"),
+        nextArrow: $(".carousel-control-next")
       });
     },
     error: function () {
-      $("#loader").hide();
-      alert("Failed to load quotes");
+      $(".loader").hide();
+      alert("Failed to load quotes.");
     }
   });
+
+  $(".loader").show();
+  $("#popular").addClass("d-none");
 });
