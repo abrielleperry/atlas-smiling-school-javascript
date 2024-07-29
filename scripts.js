e6490445d28832d7bbca01f25a82bda3ba236389
Abrielle <125398766+abrielleperry@users.noreply.github.com>
2024-07-29 16:00:54 -0500

    arrows appearing in latest but not working

--- scripts.js
+++ scripts.js
@@ -9,6 +9,7 @@ $(document).ready(function() {
 
   $("#quote-carousel").addClass("d-none");
   $("#popular-carousel").addClass("d-none");
+  $("#latest-carousel").addClass("d-none");
 
   function capitalizeFirstLetter(string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
@@ -70,7 +71,7 @@ $(document).ready(function() {
         $("#popular-carousel").removeClass("d-none");
 
         response.forEach((video, index) => {
-          const carouselInner = `
+          let carouselInner = `
           <div class="carousel-item">
               <div class="card">
                 <img src="${video.thumb_url}" class="card-img-top" alt="Video thumbnail" />
@@ -133,12 +134,69 @@ $(document).ready(function() {
     $.ajax({
       url: "https://smileschool-api.hbtn.info/latest-videos",
       method: "GET",
-      beforeSend: function () {
+      beforeSend: function() {
         showLoader();
       },
-      success: function (response) {
+      success: function(response) {
         hideLoader();
         $("#latest-carousel").removeClass("d-none");
+        response.forEach((video, index) => {
+          let carouselInner = `
+        <div class="carousel-item">
+            <div class="card">
+              <img src="${video.thumb_url}" class="card-img-top" alt="Video thumbnail" />
+              <div class="card-img-overlay text-center ">
+                <img src="images/play.png" alt="Play" width="64px" class="d-flex mx-auto play-overlay" />
+              </div>
+              <div class="card-body">
+                <h5 class="card-title font-weight-bold">${video.title}</h5>
+                <p class="card-text text-muted">${video["sub-title"]}</p>
+                <div class="creator d-flex align-items-center">
+                  <img src="${video.author_pic_url}" alt="Creator of Video" width="30px" class="rounded-circle" />
+                  <h6 class="pl-3 m-0 main-color">${video.author}</h6>
+                </div>
+                <div class="info pt-3 d-flex justify-content-between">
+                  <div class="rating row">
+                    ${getRatingStars(video.star)}
+                  </div>
+                  <span class="main-color">${video.duration}</span>
+                </div>
+            </div>
+          </div>
+        </div>
+        `;
+          $("#latest-carousel .carousel-inner").append(carouselInner);
+        });
+
+        $("#latest-carousel .carousel-inner").slick({
+          slidesToShow: 4,
+          slidesToScroll: 1,
+
+          responsive: [
+            {
+              breakpoint: 768,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1
+              }
+            },
+            {
+              breakpoint: 480,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+              }
+            }
+          ]
+        });
+      },
+      error: function() {
+        hideLoader();
+        alert("failed to load most popular tutorials api");
+      }
+    });
+  }
+  getLatestCarouselData();
 
   function getDataForSearchResults() {
     showLoader();
